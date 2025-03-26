cf9907437cf5ee741fed57f562240f473452e732
andy3400 <grayhat3400@gmail.com>
2025-03-26 23:55:06 +0900

    inital origin

---
+++ src/main/java/org/andy/coin_mate/CoinMateApplication.java
@@ -0,0 +1,13 @@
+package org.andy.coin_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/andy/coin_mate/CoinMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andy.coin_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
